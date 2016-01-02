a4355a77ecba720dbc3c28b4ba3c754e545e1539
Giannis Gasteratos <igasteratos@gmail.com>
2016-01-02 12:37:36 +0200

    abstract testing functionality in TestCaseBase

---
+++ tests/TestCaseBase.php
@@ -0,0 +1,90 @@
+<?php
+
+use Illuminate\Database\Capsule\Manager as DB;
+use Illuminate\Database\Eloquent\Model as Eloquent;
+use Orchestra\Testbench\TestCase;
+
+class TestCaseBase extends TestCase
+{
+
+    // -----------------------------------------------
+    //  Load .env Environment Variables
+    // -----------------------------------------------
+
+    public static function setUpBeforeClass()
+    {
+        if (file_exists(__DIR__.'/../.env')) {
+            $dotenv = new Dotenv\Dotenv(__DIR__.'/../');
+            $dotenv->load();
+        }
+    }
+
+    // -----------------------------------------------
+    //  Setup Database
+    // -----------------------------------------------
+
+    protected $database;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        Eloquent::unguard();
+        $database = new DB;
+        $database->addConnection([
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+        ]);
+        $database->bootEloquent();
+        $database->setAsGlobal();
+        $this->database=$database;
+
+        // Add your migrations here. ie:
+
+        // $this->database->schema()->create('TableName', function ($table) {
+        //     $table->increments('id');
+        // });        
+    }
+
+    public function tearDown() {
+    	// Drop tables here. ie:
+        // $this->database->schema()->drop('TableName');
+    }
+
+    // -----------------------------------------------
+
+    public function testDatabaseConnection()
+    {
+    	$this->assertInstanceOf(Illuminate\Database\SQLiteConnection::class, $this->database->connection());
+    }
+
+    // -----------------------------------------------
+    //  Added functionality
+    // -----------------------------------------------
+
+    protected function seeInDatabase($table, array $data, $connection = null)
+    {
+        $database = $this->database;
+
+        $count = $database->table($table)->where($data)->count();
+
+        $this->assertGreaterThan(0, $count, sprintf(
+            'Unable to find row in database table [%s] that matched attributes [%s].', $table, json_encode($data)
+        ));
+
+        return $this;
+    }
+
+    protected function notSeeInDatabase($table, array $data, $connection = null)
+    {
+        $database = $this->database;
+
+        $count = $database->table($table)->where($data)->count();
+
+        $this->assertEquals(0, $count, sprintf(
+            'Found unexpected records in database table [%s] that matched attributes [%s].', $table, json_encode($data)
+        ));
+
+        return $this;
+    }
+}    
--- tests/TranslationTest.php
+++ tests/TranslationTest.php
@@ -12,48 +12,26 @@ use igaster\TranslateEloquent\Exceptions\TranslationNotFound;
 
 use igaster\TranslateEloquent\Test\Models\Day;
 
-class TranslationTest extends TestCase
+class TranslationTest extends TestCaseBase
 {
-    // -----------------------------------------------
-    //  Load .env Environment Variables
-    // -----------------------------------------------
-
-    public static function setUpBeforeClass()
-    {
-        if (file_exists(__DIR__.'/../.env')) {
-            $dotenv = new Dotenv\Dotenv(__DIR__.'/../');
-            $dotenv->load();
-        }
-    }
 
     // -----------------------------------------------
     //  Setup Database
     // -----------------------------------------------
 
-    protected $database;
     public function setUp()
     {
         parent::setUp();
 
-        Eloquent::unguard();
-        $database = new DB;
-        $database->addConnection([
-            'driver' => 'sqlite',
-            'database' => ':memory:',
-        ]);
-        $database->bootEloquent();
-        $database->setAsGlobal();
-        $this->database=$database;
-
         // -- Set  migrations
-        $database->schema()->create('translations', function ($table) {
+        $this->database->schema()->create('translations', function ($table) {
             $table->increments('id');
             $table->string('group_id');
             $table->string('value');
             $table->string('locale', 2); // Can be any lenght!
         });
 
-        $database->schema()->create('days', function ($table) {
+        $this->database->schema()->create('days', function ($table) {
             $table->increments('id');
             $table->integer('name')->unsigned()->nullable();
             $table->boolean('weekend')->default(false);
@@ -325,33 +303,6 @@ class TranslationTest extends TestCase
             'name' => 'Πέμπτη',
         ]);
         $this->assertEquals($model->translate('el')->name, 'Πέμπτη');
-    }
-
-
-    protected function seeInDatabase($table, array $data, $connection = null)
-    {
-        $database = $this->database;
-
-        $count = $database->table($table)->where($data)->count();
-
-        $this->assertGreaterThan(0, $count, sprintf(
-            'Unable to find row in database table [%s] that matched attributes [%s].', $table, json_encode($data)
-        ));
-
-        return $this;
-    }
-
-    protected function notSeeInDatabase($table, array $data, $connection = null)
-    {
-        $database = $this->database;
-
-        $count = $database->table($table)->where($data)->count();
-
-        $this->assertEquals(0, $count, sprintf(
-            'Found unexpected records in database table [%s] that matched attributes [%s].', $table, json_encode($data)
-        ));
-
-        return $this;
     }
 
     public function test_model_delete(){
